91c38bef2a76827b51be41ca408e56f7828d035b
Alex Zou <lzou4775@uni.sydney.edu.au>
2024-04-24 12:32:14 +1000

    comment in compute_iqms test

--- pydra/tasks/mriqc/workflows/diffusion/tests/test_workflows_diffusion_base_compute_iqms.py
+++ pydra/tasks/mriqc/workflows/diffusion/tests/test_workflows_diffusion_base_compute_iqms.py
@@ -6,6 +6,7 @@ from pydra.tasks.mriqc.workflows.diffusion.base import compute_iqms
 logger = logging.getLogger(__name__)
 
 
+# Niu is not defined
 def test_compute_iqms():
     workflow = compute_iqms()
     assert isinstance(workflow, Workflow)
